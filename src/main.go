1436ded7bf097e9a2ee7d4e5a890cb0ef01605cb
tyler <tylerjnickerson@gmail.com>
2019-03-31 23:26:58 -0700

    add broadcaster

--- src/main.go
+++ src/main.go
@@ -7,15 +7,36 @@ import (
 
 type ChatStore struct {
 	chat string
-	mux sync.Mutex
+	mux  sync.Mutex
 }
 
-func (cs *ChatStore) SWrite (s string) {
+func (cs *ChatStore) SWrite(s string) {
 	defer cs.mux.Unlock()
 	cs.mux.Lock()
 	cs.chat = cs.chat + s
 }
 
+func Broadcaster() (chan chan string, chan string) {
+	register := make(chan chan string)
+	broadcast := make(chan string)
+	go func() {
+		var chans []chan string
+		for {
+			select {
+			case reg := <-register:
+				// register new channel
+				chans = append(chans, reg)
+			case message := <-broadcast:
+				// broadcast to registered channels w/o blocking
+				for _, c := range chans {
+					go func(c chan string) { c <- message }(c)
+				}
+			}
+		}
+	}()
+	return register, broadcast
+}
+
 func main() {
-	fmt.Println("nothing to see here, yet...")
+	fmt.Println("Nothing to see here, yet...")
 }
--- src/test_test.go
+++ src/test_test.go
@@ -4,26 +4,57 @@ import (
 	"testing"
 )
 
-
 func TestChatStore(t *testing.T) {
 	t.Run(
 		"Length should equal total length of all concurrent SWrite values",
-		func (t *testing.T) {
+		func(t *testing.T) {
 			chats := ChatStore{}
 			want := 1000
 			done := make(chan bool)
 			for i := 0; i < want; i++ {
-				go func (done chan bool) {
+				go func(done chan bool) {
 					chats.SWrite("a")
 					done <- true
 				}(done)
 			}
-			for i := 0; i < want; i ++ {
-				<- done
+			for i := 0; i < want; i++ {
+				<-done
 			}
 			got := len(chats.chat)
 			if want != got {
 				t.Errorf("Expected %d elements, got %d", want, got)
 			}
-	})
+		})
+}
+
+func TestBroadcast(t *testing.T) {
+	t.Run(
+		"All channels registered with broadcast should receive broadcasted messages",
+		func(t *testing.T) {
+			want := "hallo, this is dog"
+			register, broadcast := Broadcaster()
+			// register, _ := Broadcaster()
+			chans := []chan string{
+				make(chan string),
+				make(chan string),
+				make(chan string),
+			}
+			for _, chan_ := range chans {
+				register <- chan_
+			}
+			broadcast <- want
+			gots := make(chan string)
+			for _, chan_ := range chans {
+				go func(c chan string) { gots <- <-c }(chan_)
+			}
+			for i := 0; i < len(chans); i++ {
+				got := <-gots
+				if want != got {
+					t.Errorf(
+						"Expected all channels to return \"%s\" but, channel %d returned \"%s\"",
+						want, i, got,
+					)
+				}
+			}
+		})
 }
