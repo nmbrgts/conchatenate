e48c7ff15ea4592585da346c37e7012d048b952f
tyler <tylerjnickerson@gmail.com>
2019-04-13 15:47:46 -0700

    remove some tests

--- server/test_test.go
+++ server/test_test.go
@@ -38,53 +38,6 @@ func TestChatStore(t *testing.T) {
 			}
 		},
 	)
-	t.Run(
-		"Length should equal total length of all concurrent SWrite values",
-		func(t *testing.T) {
-			chats := ChatStore{}
-			want := 1000
-			done := make(chan bool)
-			for i := 0; i < want; i++ {
-				go func(done chan bool) {
-					chats.SWrite(1, "a")
-					done <- true
-				}(done)
-			}
-			for i := 0; i < want; i++ {
-				<-done
-			}
-			got := len(chats.SRead())
-			if want != got {
-				t.Errorf("Expected %d elements, got %d", want, got)
-			}
-		})
-	t.Run(
-		"Ordering of writes should be maintained between registered write IDs",
-		func(t *testing.T) {
-			chars := []string{"A", "B", "C"}
-			store := ChatStore{}
-			want := ""
-			for id, char := range chars {
-				want += strings.Repeat(char, 101)
-				store.SWrite(id, char)
-			}
-			done := make(chan bool, 300)
-			for i := 0; i < 300; i++ {
-				go func (id int, done chan bool) {
-					store.SWrite(id, chars[id])
-					done <- true
-				} (i % 3, done)
-			}
-			for i := 0; i < 300; i ++ {
-				<-done
-			}
-			got := store.SRead()
-			if want != got {
-				t.Errorf("Expected ordering of elements to match between iteratively built strin:\nwant:\n\"%s\"\ngot:\n\"%s\"",
-					want, got)
-			}
-		},
-	)
 }
 
 func TestBroadcast(t *testing.T) {
